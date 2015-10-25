825725db9d71c6c1260e8c7256f411ed50bcc68e
tonyrombo <antony.romerobonilla@gmail.com>
2015-10-25 16:17:27 -0600

    se agregan estilos a header y navigation y se agrega js para funcionamiento de la navegacion movil

--- js/main.js
+++ js/main.js
@@ -5,6 +5,9 @@ var appTabs = ( function( window, undefined ) {
     }
 })( window, undefined );
 
-
-
+if ($(window).width() < 1000) {
+	$(".show-menu").click(function () {
+		$(".header-nav-content").toggleClass("expanded");
+	});
+}
 
