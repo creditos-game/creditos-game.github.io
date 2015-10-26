53f12390752ea16dd78d82a5cbd600001b4a1ee4
tapon0509 <josue_0509@hotmail.com>
2015-10-26 14:23:12 -0600

    Modulo Credits agregado

--- js/main.js
+++ js/main.js
@@ -1,13 +1,44 @@
-var appTabs = ( function( window, undefined ) {
+var creditsModule = (function(window,undefined) {
    
-    return {
-        
-    }
-})( window, undefined );
+	dropdown = function(){
+		$(".show-menu , .header-nav-content").click(function() {
+			$(".header-nav-content").toggleClass("expanded");
 
-if ($(window).width() < 1000) {
-	$(".show-menu").click(function () {
-		$(".header-nav-content").toggleClass("expanded");
-	});
+		});
+	}
+
+	toggleDisplay = function(id){
+		$(".team-content").removeClass("visible");
+		$("#team"+id+"").addClass("visible");
+	}
+
+	scrollEfect = function(attr){
+		$('html, body').stop().animate({
+      		scrollTop: $(attr).offset().top - 100
+    	}, 800);
+    	return false;
+	}
+
+return{
+	dropdown : dropdown,
+	toggle : toggleDisplay,
+	scrollEfect : scrollEfect
 }
 
+
+})( window, undefined );
+
+
+if ($(window).width() < 1000) {
+	creditsModule.dropdown();
+}
+
+$(".display").click(function(){
+		var id = $(this).attr("id");
+		creditsModule.toggle(id);
+});
+
+$('.header-nav-item a').click(function(){ 
+	var attr = $(this).attr('href');
+	creditsModule.scrollEfect(attr);
+ });
